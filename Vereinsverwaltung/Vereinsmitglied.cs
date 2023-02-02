aba877e1bcd4e4f86146f4ca18184645e07af8e4
LinKaKling <kasimirklinger@gmx.de>
2023-02-02 14:33:58 +0100

    Projektdateien hinzufügen.

---
+++ Vereinsverwaltung/Vereinsmitglied.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vereinsverwaltung
+{
+    internal class Vereinsmitglied
+    {
+        int memberID;
+        string name;
+        string firstName;
+        string mailAdress;
+        string adress;
+        int birthYear;
+        int jobType;
+        bool useSepa;
+        int phone;
+        string country;
+        
+    }
+}
